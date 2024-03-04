11220776a197b5f714ae595bf6e55ca7dabb94ac
sergeychama24 <sergeychama24@gmail.com>
2024-03-04 21:08:15 +0300

    add animation and header

---
+++ src/components/api.js
@@ -0,0 +1,22 @@
+export const result = []
+export const key = "f35c3a30d54e474cb0bf6f725ad38c6f";
+
+export async function getGamesList(intPage) {
+    const queryParams = new URLSearchParams({
+        key: key,
+        page: intPage,
+        page_size: 40,
+        genres: "action"
+    });
+
+    const url = `https://api.rawg.io/api/games?${queryParams}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        result.push(...data.results);
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Пробросить ошибку для обработки на верхнем уровне
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import './pages/index.css'
+import {getGamesList, result} from './components/api'
 
 //
 // const app = document.querySelector('#app')
 // app.innerHTML = ``
+(async () => {
+    await getGamesList(1);
+    await getGamesList(2);
+    await getGamesList(3);
+
+    console.log(result);
+})();
